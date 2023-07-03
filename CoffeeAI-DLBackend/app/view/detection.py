cfd9b5d1333c97f84e5f481037d492a081ea0283
Kenneth Liem <kennethliem991@gmail.com>
2023-07-04 00:26:40 +0700

    Update Algorithm

--- CoffeeAI-DLBackend/app/view/detection.py
+++ CoffeeAI-DLBackend/app/view/detection.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from keras.models import load_model
 import numpy as np
+import cv2
 from flask import Blueprint, request, jsonify
 import os
 import io
@@ -34,32 +35,54 @@ def coffeeTypeDetection():
         if request.method == "POST":
             model = load_model("app/models/model.h5")
             if request.files.get("image"):
-                image = None
-                image = request.files['image'].read()
-                image = tf.keras.preprocessing.image.load_img(io.BytesIO(
-                    image), target_size=(240, 240), color_mode="grayscale")
-                image = prepare_dataset(image)
-                preds = model.predict(image, batch_size=64)
+                try:
+                    imaging = cv2.imdecode(np.fromstring(request.files['image'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+                    imaging_gray = cv2.cvtColor(imaging, cv2.COLOR_BGR2GRAY)
+                    imaging_rgb = cv2.cvtColor(imaging, cv2.COLOR_BGR2RGB)
+                    xml_data = cv2.CascadeClassifier('app/models/cascade.xml')
+                    detecting = xml_data.detectMultiScale(imaging_gray,
+                                                    minSize = (30, 30))
+                    amountDetecting = len(detecting)
+                    if amountDetecting != 0:
+                        for (a, b, width, height) in detecting:
+                            cv2.rectangle(imaging_rgb, (a, b),
+                                        (a + height, b + width),
+                                        (255, 255, 255), 5)
+                            X = a
+                            Y = b
+                            W = width
+                            H = height
 
-                if (preds[0][np.argmax(preds[0])] >= 0.8):
-                    data["coffeeType"] = labels[np.argmax(preds[0])]
-                    try:
-                        data["error"] = False
-                        data["message"] = "Success"
-                    except:
+                    cropped_image = imaging_rgb[Y:Y+H, X:X+W]
+                    cv2.imwrite('app/view/temp.jpg',cropped_image)
+                    image = None
+                    image = tf.keras.preprocessing.image.load_img(
+                        'app/view/temp.jpg', target_size=(240, 240), color_mode="grayscale")
+                    image = prepare_dataset(image)
+                    preds = model.predict(image, batch_size=64)
+                    os.remove('app/view/temp.jpg')
+                    if (preds[0][np.argmax(preds[0])] >= 0.8):
+                        data["coffeeType"] = labels[np.argmax(preds[0])]
+                        try:
+                            data["error"] = False
+                            data["message"] = "Success"
+                        except:
+                            data["error"] = True
+                            data["message"] = "Not Detected, Currently not available in our databases - Engine"
+                    else:
                         data["error"] = True
-                        data["message"] = "Under Construction! Currently not available in our databases"
-                else:
+                        data["message"] = "Not Detected, Currently not available in our databases - Engine"
+                except:
                     data["error"] = True
-                    data["message"] = "Not Detected"
+                    data["message"] = "Not Detected, Currently not available in our databases - Engine"
             else:
                 data["error"] = True
-                data["message"] = "Can't get Image"
+                data["message"] = "Can't get Image - Engine"
         else:
             data["error"] = True
-            data["message"] = "Wrong Method"
+            data["message"] = "Wrong Method - Engine"
     else:
         data["error"] = True
-        data["message"] = "Engine Offline, Please Contact service@coffeeai.online"
+        data["message"] = "Engine Offline, Please Contact service@coffeeai.online - Engine"
 
     return jsonify(data)
